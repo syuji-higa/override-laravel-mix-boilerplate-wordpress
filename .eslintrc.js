0245d1208678033bc34c81d695c3978ffafebed0
比嘉 秀士 <higa_syuuji@liginc.co.jp>
2019-04-05 15:03:51 +0900

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? ['error', {
       'allow': ['warn', 'error']
-    }] : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? ['error', {
-      'allow': ['warn', 'error']
     }] : 'off'
   }
 }
