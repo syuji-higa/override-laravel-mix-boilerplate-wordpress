31dc6eafa495b11d9562fe43427b6202cd8acf13
比嘉 秀士 <higa_syuuji@liginc.co.jp>
2019-05-12 14:43:37 +0900

    update

---
+++ wp-content/themes/input-theme-name/parts/lazy_img_uploads.php
@@ -0,0 +1,4 @@
+<img class="js-lazyloader" src="<?php echo resolve_asset_url('/images/common/placeholder.png'); ?>" data-src="<?php echo $vars[0] ?>" alt="<?php echo $vars[1] ?>">
+<noscript>
+  <img src="<?php echo resolve_asset_url('/images/'.$vars[0]); ?>" alt="<?php echo $vars[1] ?>">
+</noscript>
