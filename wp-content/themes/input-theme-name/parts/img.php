5933f0fb4f7c61cc5739e1f56c6ede99a6ae0788
比嘉 秀士 <higa_syuuji@liginc.co.jp>
2019-05-17 19:09:46 +0900

    updata

--- wp-content/themes/input-theme-name/parts/img.php
+++ wp-content/themes/input-theme-name/parts/img.php
@@ -1,1 +1,1 @@
-<img src="<?php echo resolve_asset_url('/images/common/placeholder.png'); ?>" data-set="<?php echo resolve_asset_url('/images/'.$vars[0]); ?>" alt="<?php echo $vars[1] ?>">
+<img src="<?php echo resolve_asset_url('/images/'.$vars[0]); ?>" alt="<?php echo $vars[1] ?>">
